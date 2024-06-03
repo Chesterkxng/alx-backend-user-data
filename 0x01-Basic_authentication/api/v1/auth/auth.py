0890cada05c06d1b75011d89efc4ef5743011e34
Chesterkxng <cgoita00@gmail.com>
2024-06-03 16:46:28 +0000

    save progression

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+class to manage the API authentication.
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ class definition
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ return false for the moment
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ return None for the moment
+        """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ return None for the moment
+        """
+        return None
