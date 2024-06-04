a46845b341a6de839244eb24e2388f37751b01e9
Chesterkxng <cgoita00@gmail.com>
2024-06-04 12:06:09 +0000

    format path before checking

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,13 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ return false for the moment
         """
-        return False
+        if path is None or excluded_paths is None \
+           or not excluded_paths:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """ return None for the moment
