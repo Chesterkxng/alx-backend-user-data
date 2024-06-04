350e1989cbb74b5055891d1dd5832c2bae676c37
Chesterkxng <cgoita00@gmail.com>
2024-06-04 18:50:45 +0000

    fnmatch

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,10 +15,11 @@ class Auth:
         if path is None or excluded_paths is None \
            or not excluded_paths:
             return True
-        if not path.endswith('/'):
-            path += '/'
-        if path in excluded_paths:
-            return False
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
