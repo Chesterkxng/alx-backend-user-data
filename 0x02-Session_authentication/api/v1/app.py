1bca5a5e8b46ef38a1a657ebca5860f1e3168d0c
Chesterkxng <cgoita00@gmail.com>
2024-06-05 19:07:26 +0000

    bla

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -50,19 +50,19 @@ def authenticate_user():
     """Authenticates a user before processing a request.
     """
     if auth:
-        excluded_paths = [
+        setattr(request, "current_user", auth.current_user(request))
+        excluded = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
-        if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
-            if auth_header is None:
-                abort(401)
+        if auth.require_auth(request.path, excluded):
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
+                abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
-                abort(403)
-            request.current_user = auth.current_user(request)
-
+                abort(403, description="Forbidden")
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
