fd2f9198f3d957cd436f9d8ba2e5ff4e15bf8622
Chesterkxng <cgoita00@gmail.com>
2024-06-06 11:44:33 +0000

    session init

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module for SessionAuth class
+"""
+from api.v1.auth.auth import Auth
+import uuid
+from api.v1.views.users import User
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class
+    """
+    pass
