bdbbf80692302245abcfc1c05c16898769bd0936
Chesterkxng <cgoita00@gmail.com>
2024-06-06 12:14:44 +0000

    cookies creator

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -34,3 +34,12 @@ class Auth:
         """ return None for the moment
         """
         return None
+
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request
+        """
+        if request is None:
+            return None
+
+        session_name = os.getenv('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name)
