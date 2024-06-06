1128fae25c6d7078065bcfed3e2a9541301c7994
Chesterkxng <cgoita00@gmail.com>
2024-06-06 12:45:58 +0000

    retrieve using cookies

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,3 +30,15 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ retrieve user using ssid """
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id in None:
+            return None
+        user = User.get(user_id)
+        return user
