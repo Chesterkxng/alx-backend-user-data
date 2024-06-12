96640b716a83890da9e1f80e19b0312c37cfc764
Chesterkxng <cgoita00@gmail.com>
2024-06-12 11:55:55 +0000

    edge case

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,6 +52,8 @@ def logout() -> str:
     LOG OUT
     """
     session_id = request.cookies.get("session_id")
+    if session_id is None:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
