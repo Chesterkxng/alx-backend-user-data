47d3e9509873d89f7126f5192c94d619337b84be
Chesterkxng <cgoita00@gmail.com>
2024-06-12 12:48:44 +0000

    ???

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -102,8 +102,24 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = self._generate_uuid()
-            self._db.update_user(user.id, reset_token=reset_token)
-            return user.reset_token
-        except Exception as e:
+        except NoResultFound:
             raise ValueError
+
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Uses reset token to validate update of users password"""
+        if reset_token is None or password is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_password,
+                             reset_token=None)
